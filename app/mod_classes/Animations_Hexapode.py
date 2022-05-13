0ca1fe67240b1425a112e9f02f27104d78e0ebe4
NickVanMarkes <nikito1203@gmail.com>
2022-05-13 16:05:15 +0200

    Core: advancement PID and Avance movement

--- app/mod_classes/Animations_Hexapode.py
+++ app/mod_classes/Animations_Hexapode.py
@@ -29,7 +29,8 @@ class Animations(object):
         self.pcaG = PCA9685(self.i2c, address=65)
         self.pcaD = PCA9685(self.i2c, address=66)
 
-        MAXPULSE=1890
+        self.MAXPULSE=1890
+        self.MARGEDIFFERENCE=0.5
 
         self.pcaG.frequency = 50
         self.pcaD.frequency = 50
@@ -38,49 +39,49 @@ class Animations(object):
 
         #Patte Avant Gauche
         #Hanche
-        self.havg = servo.ContinuousServo(self.pcaG.channels[0], min_pulse=1300, max_pulse=MAXPULSE)
+        self.havg = servo.ContinuousServo(self.pcaG.channels[0], min_pulse=1300, max_pulse=self.MAXPULSE)
         #Tibia
-        self.tavg = servo.ContinuousServo(self.pcaG.channels[1], min_pulse=1300, max_pulse=MAXPULSE)
+        self.tavg = servo.ContinuousServo(self.pcaG.channels[1], min_pulse=1300, max_pulse=self.MAXPULSE)
         #Pointe
-        self.pavg = servo.ContinuousServo(self.pcaG.channels[2], min_pulse=1300, max_pulse=MAXPULSE)
+        self.pavg = servo.ContinuousServo(self.pcaG.channels[2], min_pulse=1300, max_pulse=self.MAXPULSE)
 
         #Patte Milieu Gauche
         #Pointe
-        self.pmg = servo.ContinuousServo(self.pcaG.channels[4], min_pulse=1300, max_pulse=MAXPULSE)
+        self.pmg = servo.ContinuousServo(self.pcaG.channels[4], min_pulse=1300, max_pulse=self.MAXPULSE)
         #Tibia
-        self.tmg = servo.ContinuousServo(self.pcaG.channels[5], min_pulse=1300, max_pulse=MAXPULSE)
+        self.tmg = servo.ContinuousServo(self.pcaG.channels[5], min_pulse=1300, max_pulse=self.MAXPULSE)
         #Hanche
-        self.hmg = servo.ContinuousServo(self.pcaG.channels[6], min_pulse=1300, max_pulse=MAXPULSE)
+        self.hmg = servo.ContinuousServo(self.pcaG.channels[6], min_pulse=1300, max_pulse=self.MAXPULSE)
 
         #Patte Arriere Gauche
         #Hanche
-        self.harg = servo.ContinuousServo(self.pcaG.channels[12], min_pulse=1300, max_pulse=MAXPULSE)
+        self.harg = servo.ContinuousServo(self.pcaG.channels[12], min_pulse=1300, max_pulse=self.MAXPULSE)
         #Tibia
-        self.targ = servo.ContinuousServo(self.pcaG.channels[13], min_pulse=1300, max_pulse=MAXPULSE)
+        self.targ = servo.ContinuousServo(self.pcaG.channels[13], min_pulse=1300, max_pulse=self.MAXPULSE)
         #Pointe
-        self.parg = servo.ContinuousServo(self.pcaG.channels[15], min_pulse=1300, max_pulse=MAXPULSE)
+        self.parg = servo.ContinuousServo(self.pcaG.channels[15], min_pulse=1300, max_pulse=self.MAXPULSE)
 
         #Patte Avant Droite
         #Pointe
-        self.pavd = servo.ContinuousServo(self.pcaD.channels[15], min_pulse=1300, max_pulse=MAXPULSE)
+        self.pavd = servo.ContinuousServo(self.pcaD.channels[15], min_pulse=1300, max_pulse=self.MAXPULSE)
         #Tibia
-        self.tavd = servo.ContinuousServo(self.pcaD.channels[14], min_pulse=1300, max_pulse=MAXPULSE)
+        self.tavd = servo.ContinuousServo(self.pcaD.channels[14], min_pulse=1300, max_pulse=self.MAXPULSE)
         #Hanche
-        self.havd = servo.ContinuousServo(self.pcaD.channels[13], min_pulse=1300, max_pulse=MAXPULSE)
+        self.havd = servo.ContinuousServo(self.pcaD.channels[13], min_pulse=1300, max_pulse=self.MAXPULSE)
         #Patte Milieu Droite
         #Pointe
-        self.pmd = servo.ContinuousServo(self.pcaD.channels[4], min_pulse=1300, max_pulse=MAXPULSE)
+        self.pmd = servo.ContinuousServo(self.pcaD.channels[4], min_pulse=1300, max_pulse=self.MAXPULSE)
         #Tibia
-        self.tmd = servo.ContinuousServo(self.pcaD.channels[5], min_pulse=1300, max_pulse=MAXPULSE)
+        self.tmd = servo.ContinuousServo(self.pcaD.channels[5], min_pulse=1300, max_pulse=self.MAXPULSE)
         #Hanche
-        self.hmd = servo.ContinuousServo(self.pcaD.channels[6], min_pulse=1300, max_pulse=MAXPULSE)
+        self.hmd = servo.ContinuousServo(self.pcaD.channels[6], min_pulse=1300, max_pulse=self.MAXPULSE)
         #Patte Arriere Droite
         #Pointe
-        self.pard = servo.ContinuousServo(self.pcaD.channels[0], min_pulse=1300, max_pulse=MAXPULSE)
+        self.pard = servo.ContinuousServo(self.pcaD.channels[0], min_pulse=1300, max_pulse=self.MAXPULSE)
         #Tibia
-        self.tard = servo.ContinuousServo(self.pcaD.channels[1], min_pulse=1300, max_pulse=MAXPULSE)
+        self.tard = servo.ContinuousServo(self.pcaD.channels[1], min_pulse=1300, max_pulse=self.MAXPULSE)
         #Hanche
-        self.hard = servo.ContinuousServo(self.pcaD.channels[2], min_pulse=1300, max_pulse=MAXPULSE)
+        self.hard = servo.ContinuousServo(self.pcaD.channels[2], min_pulse=1300, max_pulse=self.MAXPULSE)
 
         #Hanches à 0
         self.havg.throttle=0
@@ -107,8 +108,6 @@ class Animations(object):
         self.tard.throttle=0
 
         self.initAngles=self.gyro.get_angle()
-
-
 
     def Init(self):
 
@@ -216,7 +215,7 @@ class Animations(object):
         self.pmd.throttle=0.1
         self.pard.throttle=0.1
 
-        time.sleep(1.5) #50 degrés vers le bas
+        time.sleep(2) #50 degrés vers le bas
 
         self.targ.throttle=1
         self.tavg.throttle=1
@@ -225,9 +224,10 @@ class Animations(object):
         self.tmd.throttle=-0.1
         self.tard.throttle=-0.1
 
-        self.pavg.throttle=-1
-        self.pmg.throttle=-1
-        self.parg.throttle=-1
+        self.pavg.throttle=-0.5
+        self.pmg.throttle=-0.5
+        self.parg.throttle=-0.5
+        time.sleep(3)
             
         #Maintiens
         self.tavg.throttle=0.1
@@ -236,128 +236,92 @@ class Animations(object):
         self.tavd.throttle=-0.1
         self.tmd.throttle=-0.1
         self.tard.throttle=-0.1
-        time.sleep(3)
-
-        # #Etape 3 baisser les tibias Droite et mettre une légère force sur les pointes
-
-        # time.sleep(0.003*50) #50 degrés vers le bas
-
-        # self.tavd.throttle=-0.4
-        # self.tmd.throttle=-0.4
-        # self.tard.throttle=-0.4
-
-        # self.tavg.throttle=0.3
-        # self.tmg.throttle=0.3
-        # self.targ.throttle=0.3
 
     def Avance(self):
-        angles=self.gyro.get_angle()
-        #Maintiens
-        self.tavg.throttle=0.1
-        self.tmg.throttle=0.2
-        self.targ.throttle=0.2
-        self.tavd.throttle=-0.1
-        self.tmd.throttle=-0.1
-        self.tard.throttle=-0.1
-
-        #Lever la patte avant gauche
-
+        print("AVANCE")
+        print("===========================================================")
+        #PATTES AVANT GAUCHE, MILIEU DROIT, PATTE ARRIERE GAUCHE
+        print("Lever les pattes")
         self.tavg.throttle=-1
-        self.pavg.throttle=1
+        self.tmd.throttle=1
+        self.targ.throttle=-1
+        #self.pavg.throttle=1
         time.sleep(0.003*50) #50 degrés vers le haut
         self.tavg.throttle=0
-        self.pavg.throttle=0
+        self.tmd.throttle=0
+        self.targ.throttle=0
+        #self.pavg.throttle=0
 
+        print("Tourner les hanches")
         self.havg.throttle=-1
-        time.sleep(0.003*20) #20 degrés vers l'avant
+        self.hmd.throttle=1
+        self.harg.throttle=-1
+        time.sleep(0.003*30) #30 degrés vers l'avant
         self.havg.throttle=0
-        self.pavg.throttle=-1
+        self.hmd.throttle=0
+        self.harg.throttle=0
+        #self.pavg.throttle=-0.2
         time.sleep(0.003*180) #50 degrés vers le bas
+        print("Mettre un peu de force sur les pointes")
         self.pavg.throttle=-0.1
+        self.pmd.throttle=0.1
+        self.parg.throttle=-0.1
 
+        print("baisser les pattes")
         self.tavg.throttle=1
-        while angles["x"] < self.gyro.get_angle()["x"] and angles["y"] < self.gyro.get_angle()["y"]:
-            print("Angle de départ: ", angles["x"], "\nAngles de maintenant : ",self.gyro.get_angle())
+        self.tmd.throttle=-1
+        self.targ.throttle=1
+        time.sleep(2)
         self.tavg.throttle=0.1
+        self.tmd.throttle=-0.1
+        self.targ.throttle=0.1
 
+        # #PATTE MILIEU GAUCHE
+        # #Lever la patte milieu gauche
+        # self.tmg.throttle=-1
+        # time.sleep(0.003*50) #50 degrés vers le haut
+        # self.tmg.throttle=0
 
-    # def Init4(self):
-    #     #Etape 1 baisser les tibias et légère force sur les pointes
-    #     #Tibias Gauche
+        # #rotation de la hanche
+        # self.hmg.throttle=-1
+        # time.sleep(0.003*30) #30 degrés vers l'avant
+        # self.hmg.throttle=0
 
-    #     self.tavg.throttle=0.7
-    #     #Tibias Droite
-    #     self.tavd.throttle=-1
+        # #self.pmg.throttle=-0.2
+        # time.sleep(0.003*180) #50 degrés vers le bas
+        # self.pmg.throttle=-0.1
 
-    #     #Pointes
-    #     self.pavg.throttle=-0.1
-    #     self.pavd.throttle=0.1
+        # self.tmg.throttle=1
+        # time.sleep(2)
+        # self.tmg.throttle=0.1
 
-    #     time.sleep(5) #50 degrés vers le bas
+        
 
+        self.Maintiens()
 
-
-
-    #     #Maintiens
-    #     self.tavg.throttle=0.1
-    #     self.tavd.throttle=-0.1
-
-    #     #Pointes
-    #     self.parg.throttle=-0.1
-    #     self.pard.throttle=0.1
-
-    #     #Tibias Arrière
-    #     self.targ.throttle=0.7
-    #     self.tard.throttle=-1
-    #     time.sleep(5)
-
-    #     self.targ.throttle=0.1
-    #     self.tard.throttle=-0.1
     def Maintiens(self):
+        print("MAINTIENS")
+        print("===========================================================")
         pidy=PID(0.1,0.1,0.1,setpoint=self.initAngles["y"])
-        pidx=PID(0.1,0.1,0.1,setpoint=self.initAngles["x"])
-        pidy.sample_time=1
-        pidx.sample_time=1
+        pidx=PID(0.5,0.5,0.5,setpoint=self.initAngles["x"])
+        pidy.sample_time=0.5
+        pidx.sample_time=0.5
         memooutputy=0
         memooutputx=0
         isnotplatx=True
         isnotplaty=True
 
-        while isnotplatx and isnotplaty:
+        while isnotplaty:
             angles=self.gyro.get_angle()
 
             #Axe Y
             outputy=pidy(angles["y"])
-            puissancey=0.1
-            if outputy>1:
-                puissancey=-1
-            elif outputy<-1:
-                puissancey=1
+
             if outputy!=memooutputy:
                 print("Output Y : ",outputy)
                 memooutputy=outputy
-            # if outputy<0:
-            #     self.targ.throttle=puissancey
-            #     self.tmg.throttle=puissancey
-            #     self.tavg.throttle=puissancey
-            #     self.tard.throttle=(puissancey/2)*-1
-            #     self.tmd.throttle=(puissancey/2)*-1
-            #     self.tavd.throttle=(puissancey/2)*-1
-            # elif outputy==0:
-            #     self.targ.throttle=0.1
-            #     self.tmg.throttle=0.1
-            #     self.tavg.throttle=0.1
-            #     self.tard.throttle=-0.1
-            #     self.tmd.throttle=-0.1
-            #     self.tavd.throttle=-0.1
-            # elif outputy>0:
-            #     self.tard.throttle=puissancey
-            #     self.tmd.throttle=puissancey
-            #     self.tavd.throttle=puissancey
-            #     self.targ.throttle=(puissancey/2)*-1
-            #     self.tmg.throttle=(puissancey/2)*-1
-            #     self.tavg.throttle=(puissancey/2)*-1
-
+            
+        
             if outputy<0:
                 self.targ.throttle=1
                 self.tmg.throttle=1
@@ -383,20 +347,34 @@ class Animations(object):
                 self.tavg.throttle=0.05
                 isnotplaty=True
 
-            # #Axe X
-
+            #Axe X
+        print(self.initAngles["x"])
+        print(self.initAngles["y"])
+        print(self.gyro.get_angle()["y"])
+        
+        while isnotplatx:
+            angles=self.gyro.get_angle()
+            
             outputx=pidx(angles["x"])
             if outputx!=memooutputx:
+                print("Angle de X: ",angles["x"])
                 print("Output X : ",outputx)
+                print(outputx-self.initAngles["x"])
                 memooutputx=outputx
+            difference=outputx-self.initAngles["x"]
+            
+            self.pavg.throttle=-0.2
+            self.parg.throttle=-0.1
+            self.pavd.throttle=0.1
+            self.pard.throttle=0.1
 
-            if outputx<0:
-                self.targ.throttle=1
-                self.tard.throttle=-1
-                self.tavg.throttle=0.05
-                self.tavd.throttle=-0.05
+            if difference<-self.MARGEDIFFERENCE:
+                self.targ.throttle=0.1
+                self.tard.throttle=-0.1
+                self.tavg.throttle=1
+                self.tavd.throttle=-1
                 isnotplatx=True
-            elif outputx<0.1 and outputx>-0.1:
+            elif difference<self.MARGEDIFFERENCE and difference>-self.MARGEDIFFERENCE:
                 self.targ.throttle=0.1
                 self.tmg.throttle=0.1
                 self.tavg.throttle=0.1
@@ -404,11 +382,11 @@ class Animations(object):
                 self.tmd.throttle=-0.1
                 self.tavd.throttle=-0.1
                 isnotplatx=False
-            elif outputx>0:
-                self.tavg.throttle=-1
-                self.tavd.throttle=1
-                self.targ.throttle=0.05
-                self.tard.throttle=-0.05
+            elif difference>self.MARGEDIFFERENCE:
+                self.tavg.throttle=-0.1
+                self.tavd.throttle=0.1
+                self.targ.throttle=1
+                self.tard.throttle=-1
                 isnotplatx=True
 
     #self.pcaD.deinit()
--- app/mod_classes/tests/test_Anim.py
+++ app/mod_classes/tests/test_Anim.py
@@ -5,11 +5,10 @@ import time
 sys.path.append("../")
 
 from Animations_Hexapode import Animations as Animations
-from Animations_Hexapode2 import Animations as Animations2
+
 anim= Animations()
-anim2= Animations2()
 
 anim.Init2()
 anim.Maintiens()
-#anim.Avance()
+anim.Avance()
 #anim2.Init2()
