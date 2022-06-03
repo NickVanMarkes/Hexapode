55514d06133b5f1bb1b4a0a38dd85ce8442ef4f3
NickVanMarkes <nikito1203@gmail.com>
2022-06-03 13:10:14 +0200

    Test: Check a test about the lidar thread

--- app/mod_classes/tests/test_Lidar.py
+++ app/mod_classes/tests/test_Lidar.py
@@ -4,7 +4,7 @@ import time
 
 sys.path.append("../")
 
-from app.mod_classes.LidarAsync import Lidarasync
+from mod_classes.LidarAsync import Lidarasync
 from RepeatTimer import RepeatTimer
 
 #Test du singleton
